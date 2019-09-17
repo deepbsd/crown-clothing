9b4f6f506490e92e99939964fe654ff2315b06db
David S. Jackson <deepbsd@yahoo.com>
2019-09-16 20:14:31 -0400

    added React-router-dom with Switch and withRouter with a hats component stub

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`} >
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div 
-        className="background-image" 
-        style={{
-        backgroundImage: `url(${imageUrl})`
-        }} 
-    />
+        className={`${size} menu-item`} 
+        onClick={ () => history.push(`${match.url}${linkUrl}`) } 
+    >
+        <div 
+            className="background-image" 
+            style={{
+            backgroundImage: `url(${imageUrl})`
+            }} 
+        />
     
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
@@ -18,5 +22,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem) ;
 
