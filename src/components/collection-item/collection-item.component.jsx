1e95899305e5247ca6449e4776479311dd854913
David S. Jackson <deepbsd@yahoo.com>
2019-09-17 11:53:09 -0400

    Okay, got it.  I had misnamed one of the preview classesgit add -A!  Sheesh.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -4,7 +4,7 @@ import './collection-item.styles.scss';
 
 
 const CollectionItem = ({ id, name, price, imageUrl  }) => (
-    <div className="collectionItem">
+    <div className="collection-item">
         <div 
             className="image"
             style={{ 
