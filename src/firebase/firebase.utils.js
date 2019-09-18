8adf4c1c3e0bec651b6dda0bbf8f26d2f790d892
David S. Jackson <deepbsd@yahoo.com>
2019-09-18 16:17:03 -0400

    storing some data from firebase in user info object

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,31 @@ const config = {
     appId: "1:343856483988:web:515ba7454aacbf49cfee81"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+            displayName,
+            email,
+            createdAt,
+            ...additionalData
+            })
+        } catch (error) {
+            console.log("error creating user", error.message);
+        }
+    }
+
+    return userRef;
+}
 
 firebase.initializeApp(config)
 
