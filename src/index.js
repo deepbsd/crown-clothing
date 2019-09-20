7005b13d64a7b9dc71970853995c490923d4dd11
David S. Jackson <deepbsd@yahoo.com>
2019-09-20 12:25:12 -0400

    wrote redux action for creating user and user reducer that incorporates into combined reducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 
 import './index.css';
 import App from './App';
@@ -9,10 +12,11 @@ import App from './App';
 
 
 ReactDOM.render(
-   <Router> 
-        <App />
-   </Router>,
-    
+    <Provider store={store} >
+       <Router> 
+            <App />
+       </Router>
+    </Provider>,
     document.getElementById('root')
 );
 
