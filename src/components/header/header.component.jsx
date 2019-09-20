4e60bb68a51b7b76009c3e0e142493152fed6d12
David S. Jackson <deepbsd@yahoo.com>
2019-09-20 15:46:30 -0400

    just starting the cart-icon button component and its styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+
 import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
@@ -28,6 +30,7 @@ const Header = ({ currentUser }) => (
                 SIGN IN
                 </Link>
             )}
+            <CartIcon />
         </div>
     </div>
 ); 
