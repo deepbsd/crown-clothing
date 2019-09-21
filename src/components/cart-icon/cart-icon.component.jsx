dc183cdc8b14fbdff80f5c53ac9a5f0e229c336e
David S. Jackson <deepbsd@yahoo.com>
2019-09-21 17:12:55 -0400

    finally got it.  forgot to put a {} around a function parametergit add .!

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -9,8 +9,8 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import './cart-icon.styles.scss';
 
 
-const CartIcon = (toggleCartHidden) => (
-    <div className="cart-icon" onClick={toggleCartHidden}  >
+const CartIcon = ({toggleCartHidden}) => (
+    <div className="cart-icon" onClick={ toggleCartHidden }  >
         <ShoppingIcon className="shopping-icon" />
         <span className='item-count'>0</span>
     </div>
