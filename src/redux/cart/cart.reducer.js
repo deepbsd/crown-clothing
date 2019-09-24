57a331177c24a67dbe1b857be2f5fac224fdd4d7
David S. Jackson <deepbsd@yahoo.com>
2019-09-24 11:17:07 -0400

    implemented addItem function in cart.utils.js so it updates the quantity of an item if it's already in the cart but creates the quantity of 1 if it's a new item just being added to cart for first time

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import CartActionTypes from './cart.types';
+import  { addItemToCart }  from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -11,12 +12,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden
-            }
+            };
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
-            }
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
         default: 
             return state;
     }
