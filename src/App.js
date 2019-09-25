c6cb8710adb2cff116190f030169fd7d21895d40
David S. Jackson <deepbsd@yahoo.com>
2019-09-25 12:37:31 -0400

    added checkout page to cart-dropdown component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import './App.css';
 import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
+
+import './App.css';
 
 class App extends React.Component {
 
@@ -46,6 +49,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact  path='/'  component={HomePage}  />
                     <Route path='/shop'  component={ShopPage}  />
+                    <Route exact path='/checkout' component={CheckoutPage}  />
                     <Route exact path='/signin'  
                     render={ () =>
                         this.props.currentUser ? (
