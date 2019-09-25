a5fbff0169c6140685bb0b3eab147687497ad7ab
David S. Jackson <deepbsd@yahoo.com>
2019-09-25 19:11:31 -0400

    added redux-persist and PersistGate wrapper around app to store for app and created persistor to use through out the app so we can persist cart state throughout the session regardless of reloading the page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -14,9 +14,9 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
             </div>
             <span className='name'>{name}</span>
             <span className='quantity'>
-                <div class="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+                <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
                 <span className="value">{quantity}</span>
-                <div class="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
+                <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
             </span>
             <span className='price'>${price}</span>
             <div className='remove-button' onClick={() => clearItem(cartItem)} >
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-
+import { PersistGate } from 'redux-persist/integration/react';
+import {store, persistor} from './redux/store';
 
 import './index.css';
 import App from './App';
@@ -14,7 +14,9 @@ import App from './App';
 ReactDOM.render(
     <Provider store={store} >
        <Router> 
+        <PersistGate persistor={persistor}>
             <App />
+        </PersistGate>
        </Router>
     </Provider>,
     document.getElementById('root')
