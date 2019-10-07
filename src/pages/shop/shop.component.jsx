8324adefa81fda21eb23262c06229fee08eacc0e
David S. Jackson <deepbsd@yahoo.com>
2019-10-07 18:28:36 -0400

    implemented fetch of all shop data from firebase by creating shop actions and types into the shop.reducer and modifying firebase.utils with new UPDATE_COLLECTIONS action method

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+import { updateCollections } from '../../redux/shop/shop.actions';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
@@ -11,10 +13,12 @@ class ShopPage extends React.Component {
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
+        const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
 
-        collectionRef.onSnapshot(async snapshot => { 
-            convertCollectionsSnapshotToMap(snapshot);
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
         });
     }
 
@@ -31,6 +35,12 @@ class ShopPage extends React.Component {
     }
 }
 
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => 
+        dispatch(updateCollections(collectionsMap))
+});
 
-
-export default ShopPage;
+export default connect(
+    null, 
+    mapDispatchToProps
+)(ShopPage);
