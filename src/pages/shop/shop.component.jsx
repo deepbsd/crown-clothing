f9153af6bba7ee92bbd13d511093089dd2661fc4
David S. Jackson <deepbsd@yahoo.com>
2019-10-08 11:35:11 -0400

    implement that pulls in shp data from firestore and stores it in the shop reducer from the shop page.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -25,11 +25,12 @@ class ShopPage extends React.Component {
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
 
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+        collectionRef.get().then(snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
-            this.setState({loading: false});
+            this.setState({ loading: false });
         });
+
     }
 
     render() {
