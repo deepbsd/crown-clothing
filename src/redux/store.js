fb6fe7be752769073e2a8f7f8bf33ac435fcb53a
David S. Jackson <deepbsd@yahoo.com>
2019-10-12 11:25:15 -0400

    added a root-saga file with one fetchCollectionStart saga from the shop-sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore} from 'redux-persist';
 import logger from 'redux-logger';
-//import thunk from 'redux-thunk';
+
 import createSagaMiddleware from 'redux-saga';
 
-import { fetchCollectionsStart } from './shop/shop.sagas';
+//import { fetchCollectionsStart } from './shop/shop.sagas';
 
 import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -19,7 +20,7 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
