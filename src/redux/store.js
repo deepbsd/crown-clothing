4a7298f6c888c5131d2788b4a24e338a8c454df4
David S. Jackson <deepbsd@yahoo.com>
2019-10-17 13:16:49 -0400

    trying to make root saga a bit easier to read and understand

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,7 @@ import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
+
 import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
