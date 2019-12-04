d38164c62d2db661c2d0c06eb0d99283f11f72f7
David S. Jackson <deepbsd@yahoo.com>
2019-12-04 16:04:08 -0500

    updated npm modules, updated server.js to handle stripe payment, modified stripe-button.component to push stripe token to stripe to complete payment transaction

---
+++ client/build/precache-manifest.eb3d7121d4cc61e046a48adfe2546bf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f78dc35e05c53cc348e4c1f8617cfc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56114c2991f0a21e98c2",
+    "url": "/static/css/main.3cb66314.chunk.css"
+  },
+  {
+    "revision": "ead7b0e7b8a296bc3ef4",
+    "url": "/static/js/2.23456752.chunk.js"
+  },
+  {
+    "revision": "56114c2991f0a21e98c2",
+    "url": "/static/js/main.3a81c122.chunk.js"
+  },
+  {
+    "revision": "c70724721291bce71369",
+    "url": "/static/js/runtime~main.260bcfd3.js"
+  },
+  {
+    "revision": "04fd000d2f3a940764b05409a05ba552",
+    "url": "/static/media/crown.04fd000d.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 
 const StripeCheckoutButton = ({ price }) => {
@@ -7,8 +8,21 @@ const StripeCheckoutButton = ({ price }) => {
     const publishableKey = "pk_test_LhVQdHr1BxD1M9fGFAQvjyiC00NpTso2zo";
 
     const onToken = token => {
-        console.log("token: ", token);
-        alert('Payment Successful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful');
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert(
+                'There was an issue with your payment. Please make sure you use the provided credit card.'
+            );
+        });
     }
 
     return(
