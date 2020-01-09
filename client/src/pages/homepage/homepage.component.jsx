4613b2f01322ff315827b83e5b78c2214bcc0964
David S. Jackson <deepbsd@yahoo.com>
2020-01-09 16:27:53 -0500

    updated modules(local), input Profiler and testing on Homepage load times

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
@@ -8,7 +8,15 @@ import { HomePageContainer } from './homepage.styles.jsx';
 
 const HomePage = () => (
         <HomePageContainer>
-            <Directory   />
+            <Profiler id="Directory" onRender={(id, phase, actualDuration) => {
+                console.log({
+                    id,
+                    phase,
+                    actualDuration
+                });
+            }}>
+                <Directory   />
+            </Profiler>
         </HomePageContainer>
     )
 
