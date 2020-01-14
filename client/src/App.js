99a744358298cdb9e9dd8e037ff747cf5038b9f0
David S. Jackson <deepbsd@yahoo.com>
2020-01-14 16:35:46 -0500

    removed check on currentUser to access shopping and checkout pages

--- client/src/App.js
+++ client/src/App.js
@@ -32,11 +32,7 @@ const App = ({ checkUserSession, currentUser }) => {
                     <Suspense fallback={<Spinner />}>
                         <Route exact  path='/'  component={HomePage}  />
                         <Route path='/shop' component={ShopPage} /> 
-                        <Route exact path='/checkout' 
-                            render={ () => 
-                            currentUser ? <CheckoutPage /> :  <HomePage />
-                        }
-                         />
+                        <Route exact path='/checkout' component={CheckoutPage} />
                         <Route exact path='/signin'  
                             render={ () =>
                             currentUser ? <Redirect to='/' /> :  <SignInAndSignUpPage />
