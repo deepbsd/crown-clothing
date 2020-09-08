5ecb3928b38b3cdbd6bf2037c34ad31f676f120a
David S. Jackson <deepbsd@yahoo.com>
2020-09-08 17:44:42 -0400

    updated date on test credit card in checkout component

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -41,7 +41,7 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className="test-warning">
             *Please use the following test credit card for payments*
             <br />
-            4242 4242 4242 4242 - Exp: 01/20 - CVC: 123
+            4242 4242 4242 4242 - Exp: 01/24 - CVC: 123
         </div>
 
         <StripeCheckoutButton price={total}  />
